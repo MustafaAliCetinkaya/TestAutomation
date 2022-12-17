92295e99ac99b866a2306ce080acf83a2f98dedd
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2022-12-17 19:36:47 +0300

    WebDriver setting is done

--- src/test/java/com/cydeo/tests/day1_selenium_intro/SeleniumTest.java
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/SeleniumTest.java
@@ -1,2 +1,14 @@
-package com.cydeo.tests.day1_selenium_intro;public class SeleniumTest {
+package com.cydeo.tests.day1_selenium_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://mustafaalicetinkaya.github.io/MustafaAliCetinkaya/index.htm");
+    }
 }

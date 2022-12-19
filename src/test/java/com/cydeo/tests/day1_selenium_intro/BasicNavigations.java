441bdcbc512fd3964f1f7c0b8b32d58caec73a01
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2022-12-19 22:44:46 +0300

    Automation testing is performed in for loop. Bugs are fixed. Awesome!

--- src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigations.java
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigations.java
@@ -11,22 +11,30 @@ public class BasicNavigations {
         driver.manage().window().maximize();//Makes the full screen
         //get and navigate are have same function but navigate has more options/features
 
+        //Stop code execution for 3 seconds
         Thread.sleep(3000);
-        // this will close the currently opened window by driver
+
         driver.get("https://mustafaalicetinkaya.github.io/MustafaAliCetinkaya/index.htm");
+
         //Stop code execution for 3 seconds
         Thread.sleep(3000);
         driver.navigate().to("https://mustafaalicetinkaya.github.io/MustafaAliCetinkaya/0.htm");
 
         driver.navigate().forward();
+
         //Stop code execution for 3 seconds
         Thread.sleep(3000);
+
         driver.navigate().refresh();
+
         //Stop code execution for 3 seconds
         Thread.sleep(3000);
+
         driver.navigate().back();
+
         //Stop code execution for 3 seconds
         Thread.sleep(3000);
+
         // this will close the currently opened window by driver
         driver.close();
 

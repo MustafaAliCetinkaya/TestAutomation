823be105b23d9f52b7adcee496f1127d7cab68da
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2022-12-20 15:48:31 +0300

    New automation projects are uploaded

--- src/test/java/com/cydeo/tests/locators/ShoppingCart.java
+++ src/test/java/com/cydeo/tests/locators/ShoppingCart.java
@@ -1,26 +1,23 @@
 package com.cydeo.tests.locators;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
 public class ShoppingCart {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        driver.navigate().to("https://www.a101.com.tr/");
+        driver.navigate().to("https://www.gimatgross.net/");
 
-        WebElement searchBox = driver.findElement(By.name("search_text"));
-        searchBox.sendKeys("İkbal Cezerye Fındıklı");
-        searchBox.sendKeys(Keys.ENTER);
+        driver.findElement(By.linkText("Gimat Sepeti")).click();
+        WebElement serachBox = driver.findElement(By.name("q"));
+        serachBox.sendKeys("sucuk" + Keys.ENTER);
+        driver.findElement(By.xpath("/html/body/div[6]/div[4]/div[2]/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div[1]/div/div[2]/div[3]/div[2]/button")).click();
 
-        driver.close();
     }
 }

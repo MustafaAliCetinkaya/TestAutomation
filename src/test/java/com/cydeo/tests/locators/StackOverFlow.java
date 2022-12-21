dedda2c8854bd8eddf5e1631ab802f3ce0e91316
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2022-12-22 00:53:07 +0300

    New automation projects are uploaded

--- src/test/java/com/cydeo/tests/locators/StackOverFlow.java
+++ src/test/java/com/cydeo/tests/locators/StackOverFlow.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.locators;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.List;
@@ -15,11 +17,11 @@ public class StackOverFlow {
         driver.get("https://stackoverflow.com/questions");
         List<WebElement> list = driver.findElements(By.className("question-hyperlink"));
 
-        System.out.println("Number of the current top questions: "+list.size());
+        System.out.println("Number of the current top questions: " + list.size());
 
-        int count=1;
+        int count = 1;
         for (WebElement element : list) {
-            System.out.println(count+". Top question link is : "+element.getAttribute("href"));
+            System.out.println(count + ". Top question link is : " + element.getAttribute("href"));
             count++;
             System.out.println("----------------------------------");
         }

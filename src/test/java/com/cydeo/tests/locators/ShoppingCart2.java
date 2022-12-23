aae86feb124cc48b47bc170c9da58c3b43f70504
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2022-12-23 23:34:59 +0300

    New automation projects are uploaded

--- src/test/java/com/cydeo/tests/locators/ShoppingCart2.java
+++ src/test/java/com/cydeo/tests/locators/ShoppingCart2.java
@@ -17,13 +17,23 @@ public class ShoppingCart2 {
         searchBox.sendKeys("sucuk" + Keys.ENTER);
 
         WebElement addLink = driver.findElement(By.xpath("/html/body/div[6]/div[4]/div[2]/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div[1]/div/div[2]/div[3]/div[2]/button"));
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 5; i++) {
             Thread.sleep(2000);
             //driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
             JavascriptExecutor js = (JavascriptExecutor) driver;
             js.executeScript("arguments[0].click()", addLink);
         }
-
+        Thread.sleep(2000);
+        WebElement akseker = driver.findElement(By.xpath("//input[@id='8939']"));
+        akseker.sendKeys(Keys.BACK_SPACE + "10" + Keys.ENTER);
+        driver.findElement(By.xpath("/html/body/div[6]/div[4]/div[2]/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div[3]/div/div[2]/div[3]/div[2]/div/span[1]")).click();
+        Thread.sleep(2000);
+        WebElement meram = driver.findElement(By.xpath("/html/body/div[6]/div[4]/div[2]/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div[3]/div/div[2]/div[3]/div[2]/button"));
+        Thread.sleep(2000);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].click()", meram);
+        driver.findElement(By.xpath("/html/body/div[6]/div[4]/div[2]/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div[2]/div/div[2]/div[3]/div[2]/button")).click();
+        Thread.sleep(2000);
         driver.findElement(By.linkText("alışveriş sepetinize")).click();
         driver.close();
     }

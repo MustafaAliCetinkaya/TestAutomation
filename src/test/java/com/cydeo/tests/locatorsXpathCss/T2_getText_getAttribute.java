350e78fceee3af6da01f2ed8017a0042ac0f8d46
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2022-12-26 14:28:22 +0300

    New automation projects are uploaded

--- src/test/java/com/cydeo/tests/locatorsXpathCss/T2_getText_getAttribute.java
+++ src/test/java/com/cydeo/tests/locatorsXpathCss/T2_getText_getAttribute.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class T2_getText_getAttribute {
-
     public static void main(String[] args) {
         //TC #2: NextBaseCRM, locators, getText(), getAttribute() practice
         //1- Open a Chrome browser

8b8e0c309a1339bf353024552dcac2a8963d31f0
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2022-12-27 19:29:51 +0300

    New automation projects are uploaded

--- src/test/java/com/cydeo/tests/locatorsXpathCss/A101.java
+++ src/test/java/com/cydeo/tests/locatorsXpathCss/A101.java
@@ -10,11 +10,11 @@ public class A101 {
 
         WebDriver driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
         /*- Basically this line will allow our driver to wait UP TO given time if the driver cannot immediately locate a web element.
         - We write it one time, and it is applied to the whole driver session.
         - Driver does not have to wait until 10 seconds have ended. If it finds the web element before maybe in 2 second, 5 seconds it will continue with execution.*/
-        driver.manage().deleteAllCookies();
 
         //Go to: A101 page
         driver.get("https://www.a101.com.tr/");
@@ -30,12 +30,25 @@ public class A101 {
         }
         //Find the first item
         driver.findElement(By.xpath("(//a[@title=\"GİYİM & AKSESUAR\"])[1]")).click();
-        driver.findElement(By.cssSelector("a[data-value=\"1588\"]")).click();
-        driver.findElement(By.cssSelector("a[data-value=\"1589\"]")).click();
-        //driver.findElement(By.xpath("//label[.='40-45']")).click();   Not clickable exemption is thrown
-        driver.findElement(By.cssSelector("input#attributes_integration_size40-45")).click();
-        //driver.findElement(By.cssSelector("label[for=\"attributes_integration_colourSİYAH\"]")).click();  Not clickable exemption is thrown
-        driver.findElement(By.cssSelector("input#attributes_integration_colourSİYAH")).click();
+        //driver.findElement(By.cssSelector("a[data-value=\"1588\"]")).click(); Sometimes ElementNotClickable exemption is thrown
+        driver.findElement(By.xpath("//a[@data-value='1588' and @title=\"Erkek İç Giyim\"]")).click();
+        //driver.findElement(By.cssSelector("a[data-value=\"1589\"]")).click(); Sometimes ElementNotClickable exemption is thrown
+        driver.findElement(By.xpath("//a[@data-value=\"1589\" and @title=\"Erkek Çorap\"]")).click();
+        //driver.findElement(By.xpath("//label[.='40-45']")).click();   Sometimes ElementNotClickable exemption is thrown
+
+        // Using for loop, it tries for 3 times.
+        // If the element is located for the first time then it breaks from the for loop nad comeout of the loop
+        for(int i=0; i<=2;i++){
+            try{
+                driver.findElement(By.cssSelector("li.checkbox>input#attributes_integration_size40-45")).click();
+                //driver.findElement(By.cssSelector("label[for=\"attributes_integration_colourSİYAH\"]")).click();  Sometimes ElementNotClickable exemption is thrown
+                driver.findElement(By.cssSelector("li.checkbox>input[value='SİYAH']")).click();                break;
+            }
+            catch(Exception e){
+                System.out.println(e.getMessage());
+            }
+        }
+
         driver.findElement(By.xpath("//a[@title=\"Unisex Termal Bot Çorap Siyah\"]")).click();
         //Verify the color
         String expectedColor = "Siyah";
@@ -64,10 +77,9 @@ public class A101 {
         //Go to cart
         driver.findElement(By.cssSelector("a.go-to-shop")).click();
 
-
         //Find the search box
         WebElement searchBox = driver.findElement(By.xpath("(//input[@name=\"search_text\"])[1]"));
-        searchBox.sendKeys("Albeni" + Keys.ENTER);
+        searchBox.sendKeys("Albeni"+Keys.ENTER);
         //Search for item
 
     }

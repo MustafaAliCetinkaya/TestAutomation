32ed077ad61f30a9073e68ff3537e9c7986e446e
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2022-12-31 22:30:33 +0300

    New automation projects are uploaded

--- src/test/java/com/cydeo/tests/seleniumIntro/GetMethods.java
+++ src/test/java/com/cydeo/tests/seleniumIntro/GetMethods.java
@@ -46,7 +46,24 @@ public class GetMethods {
         //6- Sayfadaki tum basliklari yazdiralim
         System.out.println(driver.findElement(By.xpath("//h1")).getText());
         System.out.println(driver.findElement(By.xpath("//h2")).getText());
+        System.out.println("-------------------------------------");
+        //1- https://www.amazon.com/ sayfasina gidelim
+        driver.get("https://amazon.com");
+        // 2- driver.getTitle() --> icinde oldugu sayfanin basligini döndürür
+        System.out.println("sayfa title : " + driver.getTitle()); //Amazon.com. Spend less. Smile more.
 
+        // 3- driver.getCurrentUrl() --> icinde oldugu sayfanin URL'ini döndürür
+        System.out.println(driver.getCurrentUrl()); // https://www.amazon.com/
+
+        // 4- driver.getPageSource(); --> icinde oldugu sayfanin kaynak kodlarini döndürür
+        System.out.println("=====================================================");
+        System.out.println(driver.getPageSource()); // arka planda calisan sayfa kodlarini yazdirir
+        System.out.println("=====================================================");
+
+        // 5- driver.getWindowHandle() --> icinde oldugu sayfanin UNIQUE hash kodunu döndürür
+        System.out.println(driver.getWindowHandle()); // CDwindow-F4A35FD5413FE52C6DEB0B03CA6DFB9F
+
+        // 6- driver.getWindowHandles() --> driver calisirken acilan tum sayfalarin UNIQUE hash kodunu döndürür
         driver.close();
 
     }

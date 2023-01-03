8c6359866f0f3afa0a2d5e8e86305870f0dba9ad
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-01-03 12:17:58 +0300

    New TestNG projects are uploaded

--- src/test/java/com/cydeo/testNG/DataProviderTest.java
+++ src/test/java/com/cydeo/testNG/DataProviderTest.java
@@ -6,16 +6,24 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class DataProviderTest {
     WebDriver driver;
+    int count=1;
+
+    @BeforeMethod
+    public void setUpMethod() {
+        System.out.println("---> BeforeMethod is running! "+count+". test is started to execute");
+        driver = WebDriverFactory.getDriver(1);
+        driver.get("https://www.google.com");
+    }
 
     @Test(dataProvider = "searchData")
     public void test(String keyword, String expectedUrl) {
-        driver = WebDriverFactory.getDriver(1);
-        driver.get("https://www.google.com");
 
         WebElement searchBox = driver.findElement(By.name("q"));
         searchBox.sendKeys(keyword + Keys.ENTER);
@@ -23,7 +31,13 @@ public class DataProviderTest {
         driver.findElement(By.xpath("(//*[contains(text(),\"https://\")])[2]")).click();
         Assert.assertEquals(driver.getCurrentUrl(),expectedUrl,keyword+" test is failed");
 
-        driver.close();
+    }
+
+    @AfterMethod
+    public void tearDownMethod(){
+        System.out.println("---> AfterMethod is running! "+count+". test is finished!");
+        count++;
+        driver.quit();
     }
 
     @DataProvider(name = "searchData")

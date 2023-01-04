a0a88de6480462e730ec3f2a088f49dc668c245c
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-01-04 22:41:05 +0300

    Bugs are fixed.

--- src/test/java/com/cydeo/selenium/locatorsXpathCss/Migros.java
+++ src/test/java/com/cydeo/selenium/locatorsXpathCss/Migros.java
@@ -1,10 +1,9 @@
 package com.cydeo.selenium.locatorsXpathCss;
 
+import com.cydeo.reusableMethods.ReusableMethods;
 import com.cydeo.reusableMethods.base.TestBase;
 import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.*;
-
-import java.util.concurrent.TimeUnit;
 
 public class Migros extends TestBase {
     public static void main(String[] args) {
@@ -17,12 +16,13 @@ public class Migros extends TestBase {
         //Find the search box and search for kek
         WebElement searchBox = driver.findElement(By.xpath("//input[@id='product-search-combobox--trigger']"));
         searchBox.sendKeys("kek" + Keys.ENTER);
-        //driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);//Wait for uploading
+
         //First item will be added after clicking the mandatory fields
         //First things first, fill the related checkboxes
         WebElement firstItem = driver.findElement(By.xpath("//div[@class='bottom-row']//fa-icon[@id='product-actions-add-to-cart-button--kekstra-konfeti-muffin-kek-kakaolu-38-g-p-4dad0d']"));
         //driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);//Wait for uploading
         firstItem.click();
+
         driver.findElement(By.xpath("//div[.='Tıkla Gel Al']")).click();
         driver.findElement(By.xpath("//span[.='İl']")).click();
         driver.findElement(By.xpath("//span[.=' Ankara ']")).click();
@@ -30,21 +30,23 @@ public class Migros extends TestBase {
         driver.findElement(By.xpath("//span[.=' Yenimahalle ']")).click();
         driver.findElement(By.xpath("//span[.='Mağaza']")).click();
         driver.findElement(By.xpath("//span[.=' ARMADA ']")).click();
-        //Find the Dr.Oetker brands group. Add the first item and then select the number of items
+
         driver.findElement(By.xpath("//label[@for=\"mat-mdc-checkbox-29-input\"]")).click();//Find the Dankek brands group.
 
         driver.findElement(By.xpath("//fa-icon[@id='product-actions-add-to-cart-button--dankek-mozaik-baton-kek-200-g-p-6b14e7']")).click();//Add first item on the list
 
+
         //Go to cart
         driver.findElement(By.cssSelector("#homepage-cart-button")).click();
         JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].click()",driver.findElement(By.xpath("//Button[.='Sepete Git']")));
-        js.executeScript("arguments[0].click()",driver.findElement(By.xpath("(//button[@aria-label=\"Sepetteki ürün sayısını arttır\"])[1]")));
+        js.executeScript("arguments[0].click()", driver.findElement(By.xpath("//Button[.='Sepete Git']")));
+        js.executeScript("arguments[0].click()", driver.findElement(By.xpath("(//button[@aria-label=\"Sepetteki ürün sayısını arttır\"])[1]")));
 
 
         //Go to payment page
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);//Wait for uploading
-        driver.findElement(By.cssSelector("button#checkout-summary-desktop-confirm-button")).click();
+        WebElement continueButton = driver.findElement(By.cssSelector("button#checkout-summary-desktop-confirm-button"));
+        ReusableMethods.waitForClickablility(continueButton, 50);
+        continueButton.click();
         driver.findElement(By.cssSelector("button.subtitle-2.text-color-grey.continue-without-register.mdc-button.mat-mdc-button.mat-unthemed.mat-mdc-button-base")).click();
 
 

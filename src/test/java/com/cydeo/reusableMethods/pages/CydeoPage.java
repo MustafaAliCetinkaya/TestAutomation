d459a991d81ead1c7c55b645978e57079653ed6b
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-01-10 18:29:59 +0300

    New automation projects have been uploaded

--- src/test/java/com/cydeo/reusableMethods/pages/CydeoPage.java
+++ src/test/java/com/cydeo/reusableMethods/pages/CydeoPage.java
@@ -12,7 +12,6 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;

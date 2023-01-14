72724f11f838d19e21c4ec0729da78872e24e875
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-01-14 09:26:28 +0300

    New automation projects have been uploaded

--- src/test/java/com/cydeo/reusableMethods/methods/ReusableMethods.java
+++ src/test/java/com/cydeo/reusableMethods/methods/ReusableMethods.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 public class ReusableMethods {
@@ -63,8 +65,8 @@ public class ReusableMethods {
 
     public static void scrollDownToElement(WebElement element) {
         ReusableMethods.waitFor(3);
-        JavascriptExecutor jse = (JavascriptExecutor) Driver.getDriver();
-        jse.executeScript("arguments[0].click();", element);
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+        js.executeScript("arguments[0].click();", element);
     }
 
 
@@ -127,4 +129,31 @@ public class ReusableMethods {
             }
         }
     }
+
+    public static void testAllLinksOnThePage(WebDriver driver) {
+        List<String> allPages = new ArrayList<>();
+        for (int i = 1; i < 50; i++) {//Firstly, inspect your site, set the breakpoint of "i" based on the number of "href tags" on the page
+            //This loop finds all href tags and store their values in allPages list
+            String eachAddress = driver.findElement(By.xpath("(//a[@href])[" + i + "]")).getAttribute("href");
+            allPages.add(eachAddress);
+        }
+
+
+        for (String each : allPages) {//This loop opens each link on a new tab
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            js.executeScript("window.open('" + each + "', '_blank');");
+        }
+
+
+        Set<String> AllHandles = driver.getWindowHandles();
+        int count = 1;
+        for (String eachHandle : AllHandles) {//This loop navigates each tab and get title/URL
+            driver.switchTo().window(eachHandle);
+            System.out.println(count + ". page title is : " + driver.getTitle() + "\n" +
+                    count + ". link is: " + driver.getCurrentUrl());
+            driver.switchTo().parentFrame();
+            count++;
+        }
+    }
+
 }

7b2d7ecd2ffee441e10334c7866ade253ac44a5e
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-01-14 09:13:16 +0300

    New automation projects have been uploaded

--- src/test/java/com/cydeo/day9/JSExecuter.java
+++ src/test/java/com/cydeo/day9/JSExecuter.java
@@ -1,7 +1,21 @@
 package com.cydeo.day9;
 
 import com.cydeo.reusableMethods.base.TestBase;
+import com.cydeo.utilities.ConfigurationReader;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
 
 public class JSExecuter extends TestBase {
 
+    @Test
+    public void scrollingDownTest() {
+        driver.get(ConfigurationReader.getProperty("practice"));
+        WebElement cydeoLink=driver.findElement(By.linkText("CYDEO"));
+
+        JavascriptExecutor js= (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView(true);",cydeoLink);
+
+    }
 }

362f79765a4b8e0020cb8488f8329f7e362ed7df
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-01-26 23:44:55 +0300

    New automation projects have been uploaded

---
+++ src/test/java/com/cydeo/reviewSessions/groupStudy/BrowserTypeTest.java
@@ -0,0 +1,37 @@
+package com.cydeo.reviewSessions.groupStudy;
+
+import com.cydeo.utilities.BrowserFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class BrowserTypeTest{
+
+    @Test
+    public static void browserTypeTest(){
+        WebDriver driver=BrowserFactory.getDriver("firefox");
+        driver.get("https://www.google.com.tr/?hl=tr");
+        String googleTitle=driver.getTitle();
+        System.out.println(googleTitle);
+
+        driver.get("https://etsy.com");
+        String etsyTitle=driver.getTitle();
+        System.out.println(etsyTitle);
+
+        driver.navigate().back();
+        verifyEquals(driver,googleTitle);
+
+        driver.navigate().forward();
+        verifyEquals(driver,etsyTitle);
+
+        driver.quit();
+    }
+
+    public static void verifyEquals(WebDriver driver, String expectedTitle){
+        String actualTitle = driver.getTitle();
+        if(actualTitle.equals(expectedTitle)){
+            System.out.println("Test is PASSED");
+        }else{
+            System.out.println("Test is FAILED");
+        }
+    }
+}
